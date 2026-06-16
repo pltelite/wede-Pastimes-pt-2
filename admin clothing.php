46caa0b5ee23353c34242526b22bd60e7a53d96b
pltelite <161828731+pltelite@users.noreply.github.com>
2026-06-16 21:04:59 +0200

    Update admin clothing.php

---
+++ admin clothing.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Manage Clothing</title>
+</head>
+<body>
+
+<h1>Manage Clothing</h1>
+
+<table border="1">
+
+<tr>
+    <th>ID</th>
+    <th>Item</th>
+    <th>Price</th>
+    <th>Actions</th>
+</tr>
+
+<tr>
+    <td>1</td>
+    <td>Vintage Jacket</td>
+    <td>R250</td>
+    <td>
+        <button>Update</button>
+        <button>Delete</button>
+    </td>
+</tr>
+
+</table>
+
+</body>
+</html>
