455caed2142463a61c0573e5e8e5caf84b67c278
pltelite <161828731+pltelite@users.noreply.github.com>
2026-06-16 20:59:14 +0200

    Update adminusers.php

---
+++ adminusers.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Manage Users</title>
+</head>
+<body>
+
+<h1>Manage Users</h1>
+
+<table border="1">
+
+<tr>
+    <th>User ID</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Actions</th>
+</tr>
+
+<tr>
+    <td>1</td>
+    <td>John</td>
+    <td>john@gmail.com</td>
+    <td>
+        <button>Edit</button>
+        <button>Delete</button>
+    </td>
+</tr>
+
+</table>
+
+</body>
+</html>
