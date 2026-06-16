8b8cb1356342ac2a82baaecc83e54931b742054e
pltelite <161828731+pltelite@users.noreply.github.com>
2026-06-16 17:29:03 +0200

    Add files via upload

---
+++ admin messages.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Communication Centre</title>
+</head>
+<body>
+
+<h1>Admin Communication</h1>
+
+<form>
+
+<label>Send To:</label><br>
+<select>
+    <option>Buyer</option>
+    <option>Seller</option>
+</select>
+
+<br><br>
+
+<label>Message:</label><br>
+<textarea rows="6" cols="40"></textarea>
+
+<br><br>
+
+<input type="submit" value="Send Message">
+
+</form>
+
+</body>
+</html>
